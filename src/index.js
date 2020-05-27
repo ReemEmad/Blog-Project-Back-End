2281703c41b1348c47927c14e110058d9bf26d04
ReemEmad <42879751+ReemEmad@users.noreply.github.com>
2020-05-27 03:02:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,6 +22,6 @@ app.get("/", (req, res) => {
 app.use((err, req, res, next) => {
   res.send(err);
 });
-app.listen(port, () =>
+app.listen(process.env.port || port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
